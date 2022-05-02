c189a487619b495b91b6e09f021a8e1dbfbf0a56
g-arnav <arnavbrookvale@gmail.com>
2022-05-02 14:02:33 -0700

    files from hw 8

---
+++ react-client/src/pages/login/Login.jsx
@@ -0,0 +1,60 @@
+import { useContext, useRef } from "react";
+import "./login.css";
+import { loginCall } from "../../loginCall";
+import { AuthContext } from "../../context/AuthContext";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  const email = useRef();
+  const password = useRef();
+
+  const { user, isFetching, error, dispatch } = useContext(AuthContext);
+  console.log("quorw");
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(dispatch);
+    loginCall(
+      { email: email.current.value, password: password.current.value },
+      dispatch
+    );
+  };
+
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">CubstartBook</h3>
+          <span className="loginDesc">Stay connected with CubstartBook.</span>
+        </div>
+        <div className="loginRight" >
+          <form className="loginBox" onSubmit={handleClick}>
+            <input
+              placeholder="Email"
+              type="email"
+              required
+              className="loginInput"
+              ref={email}
+            />
+            <input
+              placeholder="Password"
+              type="password"
+              required
+              minLength="6"
+              className="loginInput"
+              ref={password}
+            />
+            <button className="loginButton" type="submit">
+              {isFetching ? "Loading..." : error ? "Login Fail, Try Again"  : "Log In pls"}
+            </button>
+            <span className="loginForgot">Forgot Password?</span>
+            <Link to="/" style={{ textDecoration: "none" }  }>
+              <button className="loginRegisterButton">
+                Create a New Account
+              </button>
+            </Link>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
