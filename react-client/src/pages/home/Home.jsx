e8c07b87235fe758d66f3e85fd3b3234b08fae09
g-arnav <arnavbrookvale@gmail.com>
2022-05-05 15:17:18 -0700

    working logout

--- react-client/src/pages/home/Home.jsx
+++ react-client/src/pages/home/Home.jsx
@@ -3,16 +3,37 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
 import "./home.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useRef } from "react";
+import { AuthContext } from "../../context/AuthContext";
+
 
 export default function Home() {
-  return (
-    <>
-      <Topbar />
-      <div className="homeContainer">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </div>
-    </>
-  );
+
+    const { user, isFetching, error, dispatch } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const handleClick = (e) => {
+        e.preventDefault();
+        console.log(dispatch);
+
+        dispatch(
+            {type: "LOGOUT"}
+        );
+        navigate("/login");
+    };
+
+    return (
+        <>
+            <Topbar />
+            <div className="homeContainer">
+                <Sidebar />
+                <Feed />
+                <Rightbar />
+
+                <form onSubmit={handleClick} style={{ textDecoration: "none" }}>
+                    <button className="loginRegisterButton" type={"submit"} > Logout </button>
+                </form>
+            </div>
+        </>
+    );
 }
